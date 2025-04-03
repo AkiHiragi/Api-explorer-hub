b2ed70af42bf672d26289c7107b7f2c29cd44fae
Aki <akihiragi@gmail.com>
2025-04-03 16:24:56 +0300

    Создание базы данных

---
+++ API/Storage/SqliteStorage.cs
@@ -0,0 +1,22 @@
+
+public class SqliteStorage : IStorage {
+    public bool Add(Contact contact) {
+        throw new NotImplementedException();
+    }
+
+    public Contact GetContact(int id) {
+        throw new NotImplementedException();
+    }
+
+    public List<Contact> GetContacts() {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(int id) {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(int id, ContactDto contact) {
+        throw new NotImplementedException();
+    }
+}
