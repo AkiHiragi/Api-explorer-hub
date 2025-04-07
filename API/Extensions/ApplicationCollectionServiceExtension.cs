e43dd58d11f4047f64bb332129ecbaf99551a891
Aki <akihiragi@gmail.com>
2025-04-07 13:25:07 +0300

    Добавлен метод расширения

---
+++ API/Extensions/ApplicationCollectionServiceExtension.cs
@@ -0,0 +1,25 @@
+using Microsoft.OpenApi.Models;
+
+public static class ApplicationCollectionServiceExtension {
+    public static IServiceCollection AddServiceCollection(
+        this IServiceCollection services,
+        ConfigurationManager configuration) {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(opt => {
+            opt.SwaggerDoc("v1", new OpenApiInfo {
+                Title = "Api списка контактов",
+            });
+        });
+        services.AddControllers();
+        var connectionString = configuration.GetConnectionString("SqliteStringConnection");
+        services.AddSingleton<IStorage>(new SqliteStorage(connectionString));
+
+        services.AddCors(
+        opt => opt.AddPolicy("CorsPolicy", policy => {
+            policy.AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins(configuration["client"]);
+        }));
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,6 @@
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(opt => {
-    opt.SwaggerDoc("v1", new OpenApiInfo {
-        Title = "Api списка контактов",
-    });
-});
-builder.Services.AddControllers();
-var connectionString = builder.Configuration.GetConnectionString("SqliteStringConnection");
-builder.Services.AddSingleton<IStorage>(new SqliteStorage(connectionString));
-
-builder.Services.AddCors(
-opt => opt.AddPolicy("CorsPolicy", policy => {
-    policy.AllowAnyMethod()
-    .AllowAnyHeader()
-    .WithOrigins(builder.Configuration["client"]);
-}));
+builder.Services.AddServiceCollection(builder.Configuration);
 
 var app = builder.Build();
 app.UseSwagger();
